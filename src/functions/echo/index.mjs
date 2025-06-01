b0029722ee6136128e31c8ee4043d719a2c1879b
Andres Moreno <andres@andmore.dev>
2025-06-01 11:31:52 -0600

    fix packages and tests

--- src/functions/echo/index.mjs
+++ src/functions/echo/index.mjs
@@ -3,20 +3,11 @@ import { getResponse } from '../shared/apigateway.mjs';
 
 export const handler = initializePowertools(async (event) => {
   try {
-    logger.trace('Trace Log');
-    logger.debug('Debug Log');
-    logger.info('Info Log');
-    logger.warn('Warn Log');
-    logger.error('Error Log');
-    logger.critical('Critical Log');
-    logger.silent('Silent Log');
     const input = JSON.parse(event.body);
-
-    logger.flushBuffer();
 
     return getResponse(200, input);
   } catch (err) {
     logger.error(err, err.stack);
-    return getResponse(500, { message: 'Something went wrong!' })
+    return getResponse(500, { message: 'Something went wrong!' });
   }
 });
